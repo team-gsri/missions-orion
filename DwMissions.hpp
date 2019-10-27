622ee3825594abdeea5917938b135b11d33baf4f
ImplicitDeny <quentin.aymard21@gmail.com>
2019-10-27 17:08:07 +0100

    Add Lolisse with metadata

--- DwMissions.hpp
+++ DwMissions.hpp
@@ -1,4 +1,13 @@
 class DwMissions {
 	class MainMissions {
+		class Lolisse {
+			name="Batterie AA";
+			description="Nos aéronefs ne peuvent pas approcher l'île tant que la batterie AA de Lolisse est active. Faites le nécessaire pour la rendre inopérante.";
+			type="destroy";
+			location="getArray(configFile >> 'CfgWorlds' >> worldName >> 'Names' >> 'Malden_V_Lolisse' >> 'position')";
+            playable=1;
+            path="missions\lolisse\lolisse1.sqf";
+            requires=[];
+		};
 	};
 };
