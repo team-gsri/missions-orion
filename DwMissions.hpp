7896815160121d4320e20c3618f92d0be1ba4ca3
ImplicitDeny <quentin.aymard21@gmail.com>
2019-11-03 14:41:23 +0100

    Add Arudy Legacy

--- DwMissions.hpp
+++ DwMissions.hpp
@@ -14,9 +14,18 @@ class DwMissions {
 			description="Il semble que le CSAT emploie la région de Larche comme noeud central pour les communications courte-portée de ses troupes sur l'île. Perturbez les opérations ennemies en détruisant l'équipement adéquat.";
 			type="destroy";
 			location="getArray(configFile >> 'CfgWorlds' >> worldName >> 'Names' >> 'Malden_C_Larche' >> 'position')";
-            playable=1;
-            path="missions\larche\larche_legacy.sqf";
-            requires=[];
+			playable=0;
+			path="missions\larche\larche_legacy.sqf";
+			requires=[];
+		};
+		class ArudyLegacy {
+			name="Fichier d'inventaire";
+			description="Un officier du CSAT est en ce moment déployé sur Malden pour effectuer un inventaire de tout le matériel militaire présent sur l'île. Il s'agit du genre d'informations qui nous serait très utile. La dernière localisation connue de cet officier est Arudy, trouvez son ordinateur et dérobez l'inventaire qui doit y être stocké.";
+			type="download";
+			location="getArray(configFile >> 'CfgWorlds' >> worldName >> 'Names' >> 'Malden_V_Arudy' >> 'position')";
+			playable=1;
+			path="missions\arudy\arudy_legacy.sqf";
+			requires=[];
 		};
 	};
 };
